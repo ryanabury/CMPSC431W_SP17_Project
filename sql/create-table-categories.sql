1b23fbe069d84c8b52ba8f09190a28dd82b6b5bc
Ethan Raymond <e.raymond@comcast.net>
2017-02-26 13:38:25 -0500

    Added code for parent categories.

--- sql/create-table-categories.sql
+++ sql/create-table-categories.sql
@@ -1,8 +1,21 @@
 USE fusion;
 
+/* Create Table */
 CREATE TABLE Categories (
     id integer NOT NULL,
     name varchar(50) NOT NULL,
     parentID integer,
     PRIMARY KEY (id)
 );
+
+/* Populate Table */
+
+/* Parent Categories */
+INSERT INTO Categories VALUES (10000, "Automotive", -1);
+INSERT INTO Categories VALUES (20000, "Industrial", -1);
+INSERT INTO Categories VALUES (30000, "Clothing", -1);
+INSERT INTO Categories VALUES (40000, "Entertainment", -1);
+INSERT INTO Categories VALUES (50000, "Electronics", -1);
+INSERT INTO Categories VALUES (60000, "Home", -1);
+INSERT INTO Categories VALUES (70000, "Sports & Outdoors", -1);
+INSERT INTO Categories VALUES (80000, "Health & Cosmetics", -1);
