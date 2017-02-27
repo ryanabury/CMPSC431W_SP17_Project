e3302dce4e752a71728ec6af44881911b3ab26b7
Collin <demattcp@gmail.com>
2017-02-26 19:56:09 -0500

    Sales Table
    created a table for sales

---
+++ sql/sales.sql
@@ -0,0 +1,23 @@
+CREATE TABLE sales (
+	buyer_id		VARCHAR(50) NOT NULL,
+	item_id			INTEGER 	NOT NULL,
+	seller_id		VARCHAR(50) NOT NULL,
+	credit_card		INTEGER 	NOT NULL,
+	sale_price		INTEGER 	NOT NULL, 		/* monetary unit: cents */
+	status			VARCHAR(10) NOT NULL,
+	completion_date	VARCHAR(10),
+
+	PRIMARY KEY 	item_id,
+	
+	CONSTRAINT 		fk_buyer 
+	FOREIGN KEY 	(buyer_id) 
+	REFERENCES 		credit_card(username),
+	
+	CONSTRAINT 		fk_seller 
+	FOREIGN KEY 	(seller_id) 
+	REFERENCES 		reg_user(username),
+	
+	CONSTRAINT 		fk_credit 
+	FOREIGN KEY 	(credit_card) 	
+	REFERENCES 		credit_card(card_number),
+);
