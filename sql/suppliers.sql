5f37300bd0de3857ea7a5b5062bb3d4e71cbff33
cpdcode <demattcp@gmail.com>
2017-02-26 19:14:27 -0500

    Update suppliers.sql
    correcting name of postal_addr table

--- sql/suppliers.sql
+++ sql/suppliers.sql
@@ -1,15 +1,15 @@
 CREATE TABLE suppliers (
-	name 				VARCHAR(50) NOT NULL,
-	address 			VARCHAR(50),
-	password 			VARCHAR(20) NOT NULL,
+	name 			VARCHAR(50) NOT NULL,
+	address 		VARCHAR(50),
+	password 		VARCHAR(20) NOT NULL,
 	point_of_contact 	VARCHAR(20),	
-	email				VARCHAR(35) NOT NULL,
-	phone_number		CHAR(10) 	NOT NULL,
-	category			VARCHAR(20) NOT NULL,
-	yearly_revenue		INTEGER,			/* monetary unit is cents */
+	email			VARCHAR(35) NOT NULL,
+	phone_number		CHAR(10) NOT NULL,
+	category		VARCHAR(20) NOT NULL,
+	yearly_revenue		INTEGER,		/* monetary unit is cents */
 	profile_page		VARCHAR(30),		/* user specific URL extension */
 
 	PRIMARY KEY name,
 	CONSTRAINT fk_address FOREIGN KEY (address) 
-	REFERENCES postal_address(address) ON DELETE CASCADE
+	REFERENCES postal_addr(address) ON DELETE CASCADE
 );
