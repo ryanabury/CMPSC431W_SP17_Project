5f33f6ca03e98beed0997d516d6c7b609eacfe9c
ryanabury <rab5802@psu.edu>
2017-02-28 13:05:58 -0500

    Updating sales.sql

--- sql/sales.sql
+++ sql/sales.sql
@@ -1,27 +1,21 @@
+USE fusion;
+
 CREATE TABLE sales (
-	buyer_id	VARCHAR(50) 	NOT NULL,
+	buyer_id	VARCHAR(50) NOT NULL,
 	item_id		INTEGER 	NOT NULL,
-	seller_id	VARCHAR(50) 	NOT NULL,
+	seller_id	VARCHAR(50) NOT NULL,
 	credit_card	INTEGER 	NOT NULL,
 	sale_price	INTEGER 	NOT NULL, 	/* monetary unit: cents */
-	status		VARCHAR(10) 	NOT NULL,
+	transaction_status VARCHAR(10) NOT NULL,
 	completion_date	VARCHAR(10),
 
-	PRIMARY KEY 	item_id,
+	PRIMARY KEY (item_id),
 	
-	CONSTRAINT 	fk_buyer 
-	FOREIGN KEY 	(buyer_id) 
-	REFERENCES 	credit_card(username),
+	FOREIGN KEY (buyer_id) REFERENCES reg_user(username),
 	
-	CONSTRAINT 	fk_seller 
-	FOREIGN KEY 	(seller_id) 
-	REFERENCES 	reg_user(username),
+	FOREIGN KEY (seller_id) REFERENCES suppliers(supplier_id),
 	
-	CONSTRAINT 	fk_credit 
-	FOREIGN KEY 	(credit_card) 	
-	REFERENCES 	credit_card(card_number),
+	FOREIGN KEY (credit_card) REFERENCES credit_card(card_number),
 	
-	CONSTRAINT 	fk_item
-	FOREIGN KEY	(item_id)
-	REFERENCES 	sales_item(id),
+	FOREIGN KEY	(item_id) REFERENCES sales_item(id)
 );
