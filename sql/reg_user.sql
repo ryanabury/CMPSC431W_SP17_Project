be3bdc3786e6a2c36cae0dc2b1dd7ee2afae8afc
Connor <cdougherty354@gmail.com>
2017-02-28 22:17:50 -0500

    Removed inactive user table and added boolean to describe whether user is active or not. Updated table name.

--- sql/reg_user.sql
+++ sql/reg_user.sql
@@ -1,35 +1,24 @@
 USE fusion;
 
-/* active users table */
-CREATE TABLE active_user(
+CREATE TABLE user(
 	reg_id 				VARCHAR(20),
 	email 				VARCHAR(50),
-	username 			VARCHAR(50) NOT NULL,
-	first_name 			VARCHAR(25) NOT NULL,
-	last_name 			VARCHAR(25) NOT NULL,
-	password 			VARCHAR(20) NOT NULL,
+	active 				BOOLEAN,			/* active user == true, inactive user == false */
+	username 			VARCHAR(50),		/* active user must have username; enforced in front end */
+	first_name 			VARCHAR(25),		/* active user must have first_name; enforced in front end */
+	last_name 			VARCHAR(25),		/* active user must have last_name; enforced in front end */
+	password 			VARCHAR(20),		/* active user must have password; enforced in front end */
 	age 				INTEGER(3),
 	phone_num 			VARCHAR(10),
 	gender 				VARCHAR(1),
 	annual_salary 		INTEGER(12),
 
-	/* user profile page attributes*/
+	/* user profile page attributes */
 	url_ext 			VARCHAR(30),		/* user specific URL extension */
 	profile_img 		VARCHAR(512),
 	about_description 	VARCHAR(250),
 
 	/* constraints */
-	PRIMARY KEY(reg_id, email)
-);
-
-/* inactive users table */
-CREATE TABLE inactive_user(
-	reg_id 				VARCHAR(20),
-	email 				VARCHAR(50),
-	age 				INTEGER(3),
-	phone_num 			VARCHAR(10),
-	gender 				VARCHAR(1),
-	annual_salary 		INTEGER,
 	PRIMARY KEY(reg_id, email)
 );
 
@@ -40,7 +29,7 @@ CREATE TABLE postal_addr(
 	zip 				INTEGER(9),
 	reg_id 				VARCHAR(20),
 	PRIMARY KEY(street_addr, city, state, zip, reg_id),
-	FOREIGN KEY(reg_id) REFERENCES active_user(reg_id)
+	FOREIGN KEY(reg_id) REFERENCES user(reg_id)
 		ON DELETE SET NULL
 		ON UPDATE NO ACTION
 );
@@ -52,7 +41,7 @@ CREATE TABLE credit_card(
 	exp_date 			DATE 		NOT NULL,
 	reg_id 				VARCHAR(20),
 	PRIMARY KEY(card_number, reg_id),
-	FOREIGN KEY(reg_id) REFERENCES active_user(reg_id)
+	FOREIGN KEY(reg_id) REFERENCES user(reg_id)
 		ON DELETE CASCADE
 		ON UPDATE NO ACTION
 );
@@ -64,7 +53,7 @@ CREATE TABLE user_rating(
 	comment 			VARCHAR(250),
 	reg_id 				VARCHAR(20),
 	PRIMARY KEY(rid, reg_id),
-	FOREIGN KEY(reg_id) REFERENCES active_user(reg_id)
+	FOREIGN KEY(reg_id) REFERENCES user(reg_id)
 		ON DELETE CASCADE
 		ON UPDATE NO ACTION
 );
