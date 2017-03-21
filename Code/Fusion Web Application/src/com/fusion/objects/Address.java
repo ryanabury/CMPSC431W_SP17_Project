400dc1071890e5e91779248aa28e253c54ec19f0
Ethan Raymond <e.raymond@comcast.net>
2017-03-21 18:35:44 -0400

    Sorted methods for Address.

--- Code/Fusion Web Application/src/com/fusion/objects/Address.java
+++ Code/Fusion Web Application/src/com/fusion/objects/Address.java
@@ -6,9 +6,9 @@ package com.fusion.objects;
  */
 public class Address {
 
-	private String streetAddress;
 	private String city;
 	private String state;
+	private String streetAddress;
 	private byte[] zipCode;
 	
 	/**
@@ -21,32 +21,32 @@ public class Address {
 		zipCode = new byte[9];
 	}
 
-	public String getStreetAddress() {
-		return streetAddress;
-	}
-
-	public void setStreetAddress(String streetAddress) {
-		this.streetAddress = streetAddress;
-	}
-
 	public String getCity() {
 		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
 	}
 
 	public String getState() {
 		return state;
 	}
 
-	public void setState(String state) {
-		this.state = state;
+	public String getStreetAddress() {
+		return streetAddress;
 	}
 
 	public byte[] getZipCode() {
 		return zipCode;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
 	}
 
 	public void setZipCode(byte[] zipCode) {
