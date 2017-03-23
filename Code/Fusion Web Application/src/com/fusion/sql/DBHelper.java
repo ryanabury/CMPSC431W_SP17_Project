3c2f68220cc047816aff73d97faadf4f73c512f1
Ethan Raymond <e.raymond@comcast.net>
2017-03-23 11:25:43 -0400

    Added code for opening a DB helper.

--- Code/Fusion Web Application/src/com/fusion/sql/DBHelper.java
+++ Code/Fusion Web Application/src/com/fusion/sql/DBHelper.java
@@ -1,6 +1,39 @@
 package com.fusion.sql;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBHelper {
+	
+	// Database Address
+	private static final String DEFAULT_ADDRESS = "localhost";
+	private static final int DEFAULT_PORT = 3306;
+	
+	// Database Credentials
+	private static final String USERNAME = "fusion";
+	private static final String PASSWORD = "fusion_password";
+	
+	private String address;
+	private int port;
+	
+	private Connection connection;
+	
+	public DBHelper() throws DBHelperException {
+		this(DEFAULT_ADDRESS, DEFAULT_PORT);
+	}
+	
+	public DBHelper(String address, int port) throws DBHelperException {
+		try {
+			connection = DriverManager.getConnection(
+					DEFAULT_ADDRESS + ":" + DEFAULT_PORT, 
+					USERNAME, 
+					PASSWORD
+			);
+		} catch (SQLException e) {
+			throw new DBHelperException("Failed to initialize the database connection.", e);
+		}
+	}
 	
 	public static class DBHelperException extends Exception {
 		
