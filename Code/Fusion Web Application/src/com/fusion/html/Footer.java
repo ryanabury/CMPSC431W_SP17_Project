f8bdfcaf98d8060ed378aa224a6868fee9cae3e3
Ethan Raymond <e.raymond@comcast.net>
2017-04-02 19:25:15 -0400

    Updated footer.

--- Code/Fusion Web Application/src/com/fusion/html/Footer.java
+++ Code/Fusion Web Application/src/com/fusion/html/Footer.java
@@ -5,8 +5,7 @@ import static j2html.TagCreator.*;
 public class Footer {
 	
 	public static String generate() {
-		String s = p("(C) 2017, Fusion Ltd.").withClass("footer").render();
-		return s;
+		return p("(C) 2017, Fusion Ltd.").withClass("footer").render();
 	}
 	
 }
