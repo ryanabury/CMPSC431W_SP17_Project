931949a4ea66f9d3743780b578259eb9ccb480a1
jdeshaies23 <jake.deshaies@gmail.com>
2017-04-04 12:15:45 -0400

    Some sales_items/suppliers for demo
    Many more will be added.

---
+++ Code/Fusion Web Application/sql/suppliers.sql
@@ -0,0 +1,5 @@
+USE fusion;
+
+INSERT INTO suppliers (supplier_id, company_name, password, category, yearly_revenue, url_ext, banner_img, about_description) VALUES (1, "Best Buy", "electronics123", 1, 1000090, NULL, NULL, NULL);
+INSERT INTO suppliers (supplier_id, company_name, password, category, yearly_revenue, url_ext, banner_img, about_description) VALUES (2, "Radio Shack", "headphones12", 1, 100203, NULL, NULL, NULL);
+INSERT INTO suppliers (supplier_id, company_name, password, category, yearly_revenue, url_ext, banner_img, about_description) VALUES (3, "Golf Galaxy", "golfisfun21", 255, 1020302, NULL, NULL, NULL);
