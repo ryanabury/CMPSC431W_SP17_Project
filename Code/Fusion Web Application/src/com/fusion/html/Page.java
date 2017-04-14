986f2b685a27ba78e6d4a0fedff0f5fe13a9f611
Ethan Raymond <e.raymond@comcast.net>
2017-04-14 09:18:36 -0400

    Created abstract page class.

---
+++ Code/Fusion Web Application/src/com/fusion/html/Page.java
@@ -0,0 +1,17 @@
+package com.fusion.html;
+
+import j2html.tags.ContainerTag;
+
+public abstract class Page {
+	
+	private ContainerTag generateHeader() {
+		
+	}
+	
+	protected abstract ContainerTag generageBody();
+	
+	private ContainerTag generateFooter() {
+		
+	}
+
+}
