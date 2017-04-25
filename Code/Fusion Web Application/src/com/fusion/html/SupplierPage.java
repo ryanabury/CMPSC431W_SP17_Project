a642f135ce62823772cf1ab25f8d6ee44c6c4121
jdeshaies23 <jake.deshaies@gmail.com>
2017-04-25 16:10:02 -0400

    Update SupplierPage.java

--- Code/Fusion Web Application/src/com/fusion/html/SupplierPage.java
+++ Code/Fusion Web Application/src/com/fusion/html/SupplierPage.java
@@ -3,6 +3,8 @@ package com.fusion.html;
 import com.fusion.sql.DBHelper;
 import com.fusion.sql.DBHelper.DBHelperException;
 import com.fusion.objects.Supplier;
+import com.fusion.objects.SaleItem;
+import java.util.ArrayList;
 
 import j2html.tags.ContainerTag;
 import static j2html.TagCreator.*;
@@ -22,12 +24,48 @@ public class SupplierPage extends AbstractPage {
 			DBHelper db = new DBHelper();
 			
 			Supplier supplierInfo = db.getSupplier(supplierID);
+			ArrayList<SaleItem> l = db.getSaleItemBySeller(supplierID);
 			
-			return div().with(
-					h2(supplierInfo.getCompanyName()),
-					p(supplierInfo.getDescription()),
-					img().withSrc(supplierInfo.getBannerImg())
-					);
+			return div().attr("align", "center").with(
+					table()
+					.with(
+							tr().with(
+									h1(supplierInfo.getCompanyName())
+									)
+							,
+							each(l,saleItem ->
+									table().with(
+											tr().with(
+													h3(saleItem.getName()).attr("width","300")
+													),
+											tr().with(
+													td(saleItem.getDescription()).attr("width","300")
+													),
+											tr().with(
+													td("$" + Integer.toString(saleItem.getPrice())).attr("width","300")
+													),
+											tr().with(
+													td().with(
+															input()
+															.withType("submit")
+															.withValue("Buy Now")
+															.withName("buy")
+													)),
+											tr().with(
+													td().with(
+															input()
+															.withType("text")
+															.withValue("Bid Amount")
+															.withName("bid amount")
+													),
+													td().with(
+															input()
+															.withType("submit")
+															.withValue("Bid")
+															.withName("Bid")
+													))
+											))));
+									
 					
 		}catch(DBHelperException e){
 			e.getStackTrace();
