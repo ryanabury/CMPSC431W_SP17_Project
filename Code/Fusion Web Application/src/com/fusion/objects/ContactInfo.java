02e3cff411e09c721833c953f7d7fe116e0b7d19
Collin <demattcp@gmail.com>
2017-04-25 16:05:47 -0400

    Update ContactInfo.java

--- Code/Fusion Web Application/src/com/fusion/objects/ContactInfo.java
+++ Code/Fusion Web Application/src/com/fusion/objects/ContactInfo.java
@@ -3,10 +3,10 @@ package com.fusion.objects;
 public class ContactInfo {
 	
 	private String emailAddress;
-	private String postalAddress;
+	private Address postalAddress;
 	private String pointOfContact;
 	
-	public ContactInfo(String emailAddress, String address, String poc) {
+	public ContactInfo(String emailAddress, Address address, String poc) {
 		this.emailAddress = emailAddress;
 		this.postalAddress = address;
 		this.pointOfContact = poc;
@@ -16,7 +16,7 @@ public class ContactInfo {
 		return emailAddress;
 	}
 
-	public String getPostalAddress() {
+	public Address getPostalAddress() {
 		return postalAddress;
 	}
 
