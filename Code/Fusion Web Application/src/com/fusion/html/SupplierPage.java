b882e33141263a6629051f8e2b4577309bf6585d
jdeshaies23 <jake.deshaies@gmail.com>
2017-04-24 22:43:10 -0400

    Add files via upload

---
+++ Code/Fusion Web Application/src/com/fusion/html/SupplierPage.java
@@ -0,0 +1,47 @@
+package com.fusion.html;
+
+import com.fusion.sql.DBHelper;
+import com.fusion.sql.DBHelper.DBHelperException;
+import com.fusion.objects.Supplier;
+
+import j2html.tags.ContainerTag;
+import static j2html.TagCreator.*;
+
+public class SupplierPage extends AbstractPage {
+
+	private char[] supplierID;
+	
+	public SupplierPage(char[] supplierID){
+		this.supplierID = supplierID;
+	}
+	@Override
+	protected ContainerTag generateBody() {
+		//Generate body for Supplier Page
+		
+		try{
+			DBHelper db = new DBHelper();
+			
+			Supplier supplierInfo = db.getSupplier(supplierID);
+			
+			return div().with(
+					h2(supplierInfo.getCompanyName()),
+					p(supplierInfo.getDescription()),
+					img().withSrc(supplierInfo.getBannerImg())
+					);
+					
+		}catch(DBHelperException e){
+			e.getStackTrace();
+		}
+		
+		return div().with(
+				p("Error retrieving supplier.")
+				);
+	}
+
+	@Override
+	protected String pageTitle() {
+		// TODO Auto-generated method stub
+		return "Supplier Page";
+	}
+
+}
