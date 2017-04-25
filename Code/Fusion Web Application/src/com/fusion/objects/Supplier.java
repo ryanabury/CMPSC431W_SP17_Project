19cdab8bb6fa45a1065309add410c030c00b6505
jdeshaies23 <jake.deshaies@gmail.com>
2017-04-24 22:47:48 -0400

    Update Supplier.java

--- Code/Fusion Web Application/src/com/fusion/objects/Supplier.java
+++ Code/Fusion Web Application/src/com/fusion/objects/Supplier.java
@@ -5,48 +5,83 @@ public class Supplier {
 	private int supplierID;
 	private String companyName;
 	private String password;
-	private Category category;
+	private String cat;
 	private int yearlyRevenue;
 	private String urlExtention;
 	private String description;
+	private String banner_img;
 	
-	public Supplier(int supplierID, String companyName, String password, Category category, int yearlyRevenue,
-			String urlExtention, String description) {
-		this.supplierID = supplierID;
-		this.companyName = companyName;
-		this.password = password;
-		this.category = category;
-		this.yearlyRevenue = yearlyRevenue;
-		this.urlExtention = urlExtention;
-		this.description = description;
+	public Supplier() {
+		supplierID = 0;
+		companyName = "";
+		password = "";
+		cat = "";
+		yearlyRevenue = 0;
+		urlExtention = "";
+		banner_img = "";
+		description = "";
 	}
 
 	public int getSupplierID() {
 		return supplierID;
 	}
+	
+	public void setSupplierID(int supplierID) {
+		this.supplierID = supplierID;
+	}
 
 	public String getCompanyName() {
 		return companyName;
+	}
+	
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
 	}
 
 	public String getPassword() {
 		return password;
 	}
+	
+	public void setPassword(String password){
+		this.password = password;
+	}
 
-	public Category getCategory() {
-		return category;
+	public String getCategory() {
+		return cat;
+	}
+	
+	public void setCategory(String cat){
+		this.cat = cat;
 	}
 
 	public int getYearlyRevenue() {
 		return yearlyRevenue;
 	}
+	
+	public void setYearlyRevenue(int yearlyRevenue){
+		this.yearlyRevenue = yearlyRevenue;
+	}
 
 	public String getUrlExtention() {
 		return urlExtention;
+	}
+	
+	public void setUrlExtention(String urlExtention){
+		this.urlExtention = urlExtention;
+	}
+	public String getBannerImg() {
+		return banner_img;
+	}
+	
+	public void setBannerImg(String bannerImg){
+		this.banner_img = bannerImg;
 	}
 
 	public String getDescription() {
 		return description;
 	}
 	
+	public void setDescription(String description){
+		this.description = description;
+	}
 }
