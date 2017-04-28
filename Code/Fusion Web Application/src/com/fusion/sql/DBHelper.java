d6ab05845e575f7c1ba698308196a0a55873c572
jdeshaies23 <jake.deshaies@gmail.com>
2017-04-27 20:13:09 -0400

    Add getUserPurchases to DBHelper.java

--- Code/Fusion Web Application/src/com/fusion/sql/DBHelper.java
+++ Code/Fusion Web Application/src/com/fusion/sql/DBHelper.java
@@ -932,6 +932,92 @@ public class DBHelper {
 
 	}
 	
+	public ArrayList<SaleItem> getUserPurchases(char[] userID) throws DBHelperException {
+		Statement statement = null;
+		ResultSet rs = null;
+		ArrayList<SaleItem> siL = new ArrayList<SaleItem>();
+		
+		try {
+			
+			//Check for open connection
+			if(connection.isClosed()){
+				throw new DBHelperException("The connection has been closed.");
+			}
+			
+			String card_number = "";
+			
+			//Create Statement
+			statement = connection.createStatement();
+			
+			//Execute statement
+			String sql = "SELECT card_number FROM credit_card WHERE reg_id =" + new String(userID) + ";";
+			rs = statement.executeQuery(sql);
+			
+			if (!rs.next()){
+				System.out.println("The user does not have a credit card in the system.");
+			} else {
+				card_number = rs.getString("card_number");
+			}
+			
+			statement.close();
+			rs.close();
+			
+			statement = null;
+			rs = null;
+			
+			ArrayList<char[]> itemIDs = new ArrayList<char[]>();
+			
+			if (card_number != null){
+				statement = connection.createStatement();
+				
+				sql = "SELECT item_id FROM sales_transaction WHERE credit_card =" + card_number + ";";
+				rs = statement.executeQuery(sql);
+				
+				if (!rs.next()){
+					System.out.println("No items have been purchased with that credit card.");
+				} else {
+					itemIDs.add(rs.getString("item_id").toCharArray());
+				}
+				
+				statement.close();
+				rs.close();
+				
+				statement = null;
+				rs = null;
+				
+				int i = 0;
+				
+				for(i = 0; i < itemIDs.size(); i++){
+					statement = connection.createStatement();
+					
+					sql = "SELECT * FROM sale_items WHERE id=" + new String(itemIDs.get(i)) + ";";
+					rs = statement.executeQuery(sql);
+					
+					while(rs.next()){
+						siL.add(new SaleItem(rs.getInt("id"),rs.getString("name"),rs.getInt("seller"),
+								rs.getInt("price"),rs.getInt("reservePrice"), rs.getInt("quantity"),
+								getCategory(Integer.parseInt(rs.getString("category"))),rs.getString("detailedDescriptionURL"),
+								SaleItem.TypeOfSale.fromInt(Integer.parseInt(rs.getString("typeOfSale"))),
+								rs.getString("description")));
+					}
+					
+					statement.close();
+					rs.close();
+					
+					statement = null;
+					rs = null;
+				}
+			} else {
+				System.out.println("Error with credit card value.");
+			}
+			
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return siL;
+	}
+	
 	public void close() {
 		try {
 			connection.close();
